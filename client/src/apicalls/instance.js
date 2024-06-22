17d0e5252db8e85c3cc9aa9775f1c3cff851eee8
FreJuni <websitepsw11@gmail.com>
2024-06-22 13:56:11 +0630

    change url

--- client/src/apicalls/instance.js
+++ client/src/apicalls/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://e-comerce-2-api.onrender.com'
 });
 
 const Token = () => {
